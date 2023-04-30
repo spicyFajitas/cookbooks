607cff9379ccaf2d2f8c0c6f2c070b6596866c80
Adam F <68786858+afulton101222@users.noreply.github.com>
2023-04-30 00:50:52 -0500

    init after install script for various servers

---
+++ userscripts/bash/after_install_ansible_server.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# first time setup script for ansible server
+
+
+echo -e "
+
+  # #     # #         #                                          # #     # #
+  # #     # #        # #   ###### ##### ###### #####             # #     # #
+####### #######     #   #  #        #   #      #    #          ####### #######
+  # #     # #      #     # #####    #   #####  #    #            # #     # #
+####### #######    ####### #        #   #      #####           ####### #######
+  # #     # #      #     # #        #   #      #   #             # #     # #
+  # #     # #      #     # #        #   ###### #    #            # #     # #
+
+  # #     # #      ###                                               # #     # #
+  # #     # #       #  #    #  ####  #####   ##   #      #           # #     # #
+####### #######     #  ##   # #        #    #  #  #      #         ####### #######
+  # #     # #       #  # #  #  ####    #   #    # #      #           # #     # #
+####### #######     #  #  # #      #   #   ###### #      #         ####### #######
+  # #     # #       #  #   ## #    #   #   #    # #      #           # #     # #
+  # #     # #      ### #    #  ####    #   #    # ###### ######      # #     # #
+
+  # #     # #       #####                                       # #     # #
+  # #     # #      #     #  ####  #####  # #####  #####         # #     # #
+####### #######    #       #    # #    # # #    #   #         ####### #######
+  # #     # #       #####  #      #    # # #    #   #           # #     # #
+####### #######          # #      #####  # #####    #         ####### #######
+  # #     # #      #     # #    # #   #  # #        #           # #     # #
+  # #     # #       #####   ####  #    # # #        #           # #     # #
+
+"
+####################
+
+echo -e "\n\n"
+
+
+mkdir -p ~/.ssh
+
+echo "################################################################################"
+echo -e "adding Adam's macbook SSH key to this machine"
+echo "################################################################################"
+echo -e "\n\n"
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICKlWUHi61l2BmJKJTSU/uC+fa2H1a73VdmDxmcwCZRo adam@Fajita-MacBook.lan" >> ~/.ssh/authorized_keys
+
+
+echo "################################################################################"
+echo -e "adding ansible-server's SSH key to this machine"
+echo "################################################################################"
+echo -e "\n\n"
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqz7vI3Iur9UmtKiXpH6zNkieG65xjETZuwdUHDTArw root@ansible-server" >> ~/.ssh/authorized_keys
+
+sleep 5
+echo "################################################################################"
+echo -e "running apt update -y"
+echo "################################################################################"
+echo -e "\n\n"
+sleep 2
+sudo apt update
+
+echo -e "\n\n"
+echo "################################################################################"
+echo -e "installing ansible, git, neofetch, net-tools, and vim"
+echo "################################################################################"
+echo -e "\n\n"
+sleep 5
+sudo apt --assume-yes install ansible vim git net-tools neofetch
+
+echo -e "\n\n"
+echo "################################################################################"
+echo -e "enabling SSH"
+echo "################################################################################"
+echo -e "\n\n"
+
+systemctl enable ssh && systemctl start ssh
+
+echo -e "\n\n"
+echo "################################################################################"
+echo -e "cloning Cookbooks repo and Docker Testing repo"
+echo "################################################################################"
+echo -e "\n\n"
+git clone https://github.com/spicyFajitas/cookbooks.git
+git clone https://github.com/spicyFajitas/KinD-testing.git
+
+echo -e "\n\n"
+echo "################################################################################"
+echo -e "running apt upgrade -y"
+echo "################################################################################"
+echo -e "\n\n"
+sudo apt upgrade -y
