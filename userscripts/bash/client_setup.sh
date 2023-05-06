73dba0a057c1f9c83a75686a06d7c457f3b47c77
Adam F <68786858+spicyFajitas@users.noreply.github.com>
2023-05-06 13:19:33 -0500

    [feat] creating uptime kuma role (#6)
    * backup keybindings
    
    * modify setup scirpts
    
    rename client setup script and add SSH key of new laptop
    
    * [feat] init uptime kuma ansible role
    
    * [fix] add IP to cloud kuma host
    
    * remove usage section netbox readme
    
    * remove semicolons from hosts
    
    * add tasks to uptime kuma
    
    * update inventory syntax
    
    * syntax
    
    * created to do list
    
    * [fix] create uptime kuma directory if it does not exist
    
    * genericise restart handlers
    
    * syntax docker compose
    
    * [fix] restart task handler name incorrect
    
    * [fix] make base dir and contianer name consistent

--- userscripts/bash/client_setup.sh
+++ userscripts/bash/client_setup.sh
@@ -36,18 +36,18 @@ echo -e "\n\n"
 
 mkdir -p ~/.ssh
 
-echo "################################################################################"
-echo -e "adding Adam's macbook SSH key to this machine"
-echo "################################################################################"
-echo -e "\n\n"
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICKlWUHi61l2BmJKJTSU/uC+fa2H1a73VdmDxmcwCZRo adam@Fajita-MacBook.lan" >> ~/.ssh/authorized_keys
+# echo "################################################################################"
+# echo -e "adding Adam's macbook SSH key to this machine"
+# echo "################################################################################"
+# echo -e "\n\n"
+# echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICKlWUHi61l2BmJKJTSU/uC+fa2H1a73VdmDxmcwCZRo adam@Fajita-MacBook.lan" >> ~/.ssh/authorized_keys
 
 
-echo "################################################################################"
-echo -e "adding ansible-server's SSH key to this machine"
-echo "################################################################################"
-echo -e "\n\n"
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqz7vI3Iur9UmtKiXpH6zNkieG65xjETZuwdUHDTArw root@ansible-server" >> ~/.ssh/authorized_keys
+# echo "################################################################################"
+# echo -e "adding ansible-server's SSH key to this machine"
+# echo "################################################################################"
+# echo -e "\n\n"
+# echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqz7vI3Iur9UmtKiXpH6zNkieG65xjETZuwdUHDTArw root@ansible-server" >> ~/.ssh/authorized_keys
 
 sleep 5
 echo "################################################################################"
@@ -64,6 +64,24 @@ echo "##########################################################################
 echo -e "\n\n"
 sleep 5
 sudo apt --assume-yes install vim git net-tools neofetch
+
+echo -e "\n\n"
+echo "################################################################################"
+echo -e "installing docker"
+echo "################################################################################"
+echo -e "\n\n"
+sleep 5
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get install ca-certificates curl gnupg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 echo -e "\n\n"
 echo "################################################################################"
