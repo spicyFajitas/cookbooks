180aa8be2ee239b12400ec21733e4744778a4e55
Adam Fulton <afulton@xes-inc.com>
2023-07-05 12:26:05 -0500

    feat(bash): ✨ database conversion scripts
    scripts uploaded that convert tables to InnoDB engine and utf8mb4 character set

---
+++ userscripts/bash/db_innodb_conversion.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+###############################################################################
+## Author: Adam Fulton @spicyFajitas
+## Date:   2023-07-05
+## Descr:  converts database engine from whatever engine to InnoDB
+###############################################################################
+
+# MySQL credentials
+MYSQL_USER="your_username"
+MYSQL_PASSWORD="your_password"
+
+# Connect to MySQL and retrieve a list of all databases
+DATABASES=$(mysql -e "SHOW DATABASES;" | grep -Ev "(information_schema|performance_schema)")
+
+# Iterate over each database and convert tables from MyISAM to InnoDB
+for DATABASE in $DATABASES; do
+  TABLES=$(mysql -N -B -e "USE $DATABASE; SHOW TABLES;" | tr '\n' ' ')
+#  TABLES=$(mysql -u $MYSQL_USER -p $MYSQL_PASSWORD -N -B -e "USE $DATABASE; SHOW TABLES;" | tr '\n' ' ')
+  for TABLE in $TABLES; do
+    echo "Converting table $TABLE in database $DATABASE..."
+    mysql -e "USE $DATABASE; ALTER TABLE $TABLE ENGINE=InnoDB;"
+#    mysql -u $MYSQL_USER -p $MYSQL_PASSWORD -e "USE $DATABASE; ALTER TABLE $TABLE CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+
+  done
+done
+
+echo "Conversion complete!"
---
+++ userscripts/bash/db_utf8mb4_conversion.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+###############################################################################
+## Author: Adam Fulton @spicyFajitas
+## Date:   2023-07-05
+## Descr:  converts database character set from whatever to utf8mb4
+###############################################################################
+
+# Connect to MySQL and retrieve a list of all databases
+DATABASES=$(mysql -e "SHOW DATABASES;" | grep -Ev "(information_schema|performance_schema)")
+
+# Iterate over each database and convert tables to utf8mb4 character set
+for DATABASE in $DATABASES; do
+  TABLES=$(mysql -N -B -e "USE $DATABASE; SHOW TABLES;" | tr '\n' ' ')
+#  TABLES=$(mysql -u $MYSQL_USER -p $MYSQL_PASSWORD -N -B -e "USE $DATABASE; SHOW TABLES;" | tr '\n' ' ')
+  for TABLE in $TABLES; do
+    echo "Converting table $TABLE in database $DATABASE..."
+    mysql -e "USE $DATABASE; ALTER TABLE $TABLE CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+  done
+done
+
+# Set default character set to utf8mb4 for each database
+for DATABASE in $DATABASES; do
+  echo "Setting default character set to utf8mb4 for database $DATABASE..."
+  mysql -e "USE $DATABASE; ALTER DATABASE $DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+#  mysql -u $MYSQL_USER -p $MYSQL_PASSWORD -e "USE $DATABASE; ALTER DATABASE $DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+done
+
+echo "Conversion complete!"
